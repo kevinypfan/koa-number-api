b5eb53163170bcc7fdf873c5710892dfe6b42edf
KevinFan <kevinypfan@gmail.com>
2020-11-20 18:26:31 +0800

    +: first commit

---
+++ server.js
@@ -0,0 +1,40 @@
+require("dotenv").config();
+
+const Koa = require("koa");
+const Router = require("@koa/router");
+const MongoClient = require("mongodb").MongoClient;
+
+const app = new Koa();
+const router = new Router();
+
+const mongoClient = new MongoClient(process.env.MONGODB_URL, {
+  useUnifiedTopology: true,
+});
+
+let client = null;
+
+router.get("/recent-numbers", async (ctx, next) => {
+  try {
+    const db = await client.db(process.env.MONGODB_DATABASE);
+    const collections = await db.listCollections().toArray();
+
+    const getDatePromises = collections.map((col) => {
+      return db.collection(col.name).findOne({}, { sort: { _id: -1 } });
+    });
+    ctx.body = await Promise.all(getDatePromises);
+  } catch (error) {
+    console.log(error);
+    ctx.body = error;
+    ctx.status = 400;
+  }
+});
+
+// add the router to our app
+app.use(router.routes());
+app.use(router.allowedMethods());
+
+// start the server
+mongoClient.connect().then((result) => {
+  app.listen(process.env.PORT);
+  client = result;
+});
